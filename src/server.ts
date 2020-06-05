e407d8380bcee4f2fb08243b751527e30dc54d2d
abeRC <aeciobeltrao@usp.br>
2020-06-05 05:25:19 -0300

    server v2

--- src/server.ts
+++ src/server.ts
@@ -1,70 +1,67 @@
 import express from 'express';
-/*Quando usamos o TypeScrypt, além de instalar a biblioteca, também precisamos instalar suas definições de tipos.
-Algumas bibliotecas já vem com isso, mas não no caso do express.
-ProTip: ´npm install @types/express -D´ instala essas definições de tipo como dependências de desenvolvimento,
-indicando que não é necessária para produção.
 
-Outra coisa: toda vez que criamos uma aplicação TypeScript, precisamos de um arquivo de configuração
-indicando quais recursos do TypeScript vamos utilizar.
-
-
-
-Execução: npx ts-node-dev src/server.ts OU rode o script em package.json com npm run batata OU yarn batata.
+/*Execução: npx ts-node-dev src/server.ts OU rode o script em package.json com npm run batata OU yarn batata.
 ts-node-dev está em node_modules/.bin e vem do pacote node-dev para que a gente
 não tenha que reiniciar manualmente o servidor toda vez que mudamos alguma coisa.*/
 
-const app = express();
-app.get("/users", (request, response) => {
-    /*Rota (endereço completo da requisição): localhost:3333/users | recurso (entidade acessada): /users 
-    GET: buscar informações do back-end
-    POST: criar informações no back-end
-    PUT: atualizar informação existente no back-end
-    DELETE: remover informações do back-end
-    
-    POST http://localhost:3333/users ---> listar usuários
-    POST http://localhost:3333/000000247 ---> buscar dados do usuário de ID 247 
-    
-    Testar requisições: Insomnia (so you can REST)*/
-    console.log("listagem de usuários");
+const users = [
+  'Sir Lancelot the Brave',
+  'Sir Galahad the Pure',
+  'Sir Bedevere the Wise'
+];
 
-    //response.send("Hello, world!");
-    //response.status(411).send("Oh..."); //A chamada para status() seta o status e o send() envia a resposta.
-    response.json([ //missing a return?
-      'Sir Lancelot the Brave',
-      'Sir Galahad the Pure',
-      'Sir Bedevere the Wise'
-    ]); //Lembra que queríamos usar JSON?
+const app = express();
+
+app.use(express.json()); //Returns (Express) middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
+
+//IMPORTANTE: .json() é uma função; não esqueça dos devidos parêntesis. :)
+
+
+
+/*Rota (endereço completo da requisição): localhost:3333/users | recurso (entidade acessada): /users 
+GET: buscar informações do back-end
+POST: criar informações no back-end
+PUT: atualizar informação existente no back-end
+DELETE: remover informações do back-end
+
+POST http://localhost:3333/users ---> listar usuários
+POST http://localhost:3333/000000247 ---> buscar dados do usuário de ID 247
+GET http://localhost:3333/users?search=dev ---> listar usuários pela palavra 'dev'
+
+Testar requisições: Insomnia (so you can REST)*/
+app.get("/users", (request, response) => {
+    const search = String(request.query.search); //Se houver mais de um parâmetro de consulta, query.search será um array.
+    console.log("query parameter: "+request.query.search +" | search: "+search);
+    
+    //se houver parametro de busca
+    const filteredUsers = search ? (users.filter(user => user.includes(search)) ) : users;
+    
+    response.json(filteredUsers); //Lembra que queríamos usar JSON?
 });
 
-//https://www.w3schools.com/js/js_arrow_function.asp
-/* versão 1
-hello = function() {
-  return "Hello World!";
-}  
+/*Request parameters: O dois-pontos indica um parâmetro de requisição, que vem na própria rota e identifica um recurso.
+A rota (quase sempre) não sobrevive sem esse parâmetro.
 
-//versão 2
-hello = () => {
-  return "Hello World!";
-}
+Query parameters: Parâmetros opcionais para filtrar, paginar, etc. 
+O nome é definido pela consulta em si, diferentemente dos parâmetros de requisição.
 
-//versão 3
-hello = () => "Hello World!";
+Request body: parâmetros para criação e atualização de informações no back-end.*/
 
-//com parâmetros
-hello = (val) => "Hello " + val; 
+app.get("/users/:foo", (request, response) => {
+    const id = Number(request.params.foo);
 
+    const user = users[id];
 
-Cuidado com `this`!
-In regular functions the this keyword represented the object that called the function, 
-which could be the window, the document, a button or whatever.
-With arrow functions the this keyword always represents the object that defined the arrow function.*/
+    return response.json(user);
+})
 
 app.post("/users", (request, response) => {
+    const data = request.body;
     const user = {
-        nome: "Galahad",
-        titulo: "Sir",
-        qualidade: "the Pure"
-    }
+        name: data.name,
+        title: data.title,
+        quality: data.quality
+    };
 
     return response.json(user);
 })
