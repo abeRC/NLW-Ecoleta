57de4d4dbfbe9520213fbeb476631e0c431ce727
abeRC <aeciobeltrao@usp.br>
2020-06-06 04:47:04 -0300

    CORS configurado para o routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
+import cors from "cors";
 
 /*Métodos padrão: index (listagem), show (exibir um único registro), create/store, update, delete/destroy*/
 const routes = express.Router();
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
+
+routes.use(cors({
+    origin: "http://localhost:3000"
+})); 
+/*CORS = Cross-origin resource sharing
+Define os domínios que terão acesso à nossa aplicação. Daria para definir um objeto com origin para controlar isso.
+Se nao ativarmos isso, o nosso front-end nao tera como acessar o nosso back-end!
+(O front esta no port 3000 e o back esta no port 3333.)*/
+
 
 /*Lista todos os itens cadastrados. */
 routes.get("/items", itemsController.index);
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,12 @@ const app = express();
 app.use(express.json()); //IMPORTANTE: .json() é uma função; não esqueça dos devidos parêntesis. :)
 app.use(routes);
 app.use("/uploads", express.static(path.resolve(__dirname,"..", "uploads"))); //para servir recursos estáticos como imagens
-app.use(cors()); //Define os domínios que terão acesso à nossa aplicação. Daria para definir um objeto com origin para controlar isso.
-//CORS = Cross-origin resource sharing
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+/*CORS = Cross-origin resource sharing
+Define os domínios que terão acesso à nossa aplicação. Daria para definir um objeto com origin para controlar isso.
+Se nao ativarmos isso, o nosso front-end nao tera como acessar o nosso back-end!
+(O front esta no port 3000 e o back esta no port 3333.)*/
 
 app.listen(3333);
