301b9c408a0eefefaa520aab944a1f588ebf953e
abeRC <aeciobeltrao@usp.br>
2020-06-07 09:55:56 -0300

    image upload ok

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,16 @@ import express from "express";
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
 import cors from "cors";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 /*Métodos padrão: index (listagem), show (exibir um único registro), create/store, update, delete/destroy*/
 const routes = express.Router();
+
+/*multer + config 
+Lembre de passar isso junto com as rotas que envolvem uplaod de imagens!*/
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
@@ -23,11 +30,11 @@ routes.get("/items", itemsController.index);
 /*Lista pontos filtrando por cidade/estado/itens.*/
 routes.get("/points", pointsController.index);
 
-/*Cadastra um novo ponto de coleta.*/
-routes.post("/points", pointsController.create);
-
 /*Exibe informações sobre um ponto específico. */
 routes.get("/points/:id", pointsController.show);
+
+/*Cadastra um novo ponto de coleta.*/
+routes.post("/points", upload.single("image"),pointsController.create);
 
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 
 const app = express();
 
+
 app.use(express.json()); //IMPORTANTE: .json() é uma função; não esqueça dos devidos parêntesis. :)
 app.use(routes);
 app.use("/uploads", express.static(path.resolve(__dirname,"..", "uploads"))); //para servir recursos estáticos como imagens
@@ -14,6 +15,6 @@ app.use(cors({
 /*CORS = Cross-origin resource sharing
 Define os domínios que terão acesso à nossa aplicação. Daria para definir um objeto com origin para controlar isso.
 Se nao ativarmos isso, o nosso front-end nao tera como acessar o nosso back-end!
-(O front esta no port 3000 e o back esta no port 3333.)*/
+(O front está no port 3000 e o back está no port 3333.)*/
 
 app.listen(3333);
