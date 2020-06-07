f3eb83f51f0780d7ec40fa87ed8b858474871c43
abeRC <aeciobeltrao@usp.br>
2020-06-07 07:57:37 -0300

    license

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -12,7 +12,9 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`
+                image_url: `http://192.168.100.28:3333/uploads/${item.image}`
+                /*Isto foi alterado para testar o app do React Native.
+                Lembre de mudar de volta! ^^ */
             };
         });
         return response.json(serializedItems);
